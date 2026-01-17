0c4ed0d8634bbc54d8e41c6240c79018b4b44f50
João Pedro <joaoxstone@gmail.com>
2026-01-17 15:30:56 -0300

    chore: card detalhado

--- frontend/response-factory/src/components/ProductCard.vue
+++ frontend/response-factory/src/components/ProductCard.vue
@@ -1,15 +1,16 @@
 <template>
-  <div class="card">
-    <strong>{{ product.nome }}</strong>
-    <span>R$ {{ product.valor }}</span>
-    <button class="danger" :disabled="deleting" @click="$emit('delete')">
-      {{ deleting ? 'Deletando...' : 'Deletar' }}
-    </button>
-  </div>
+    <div class="card">
+        <strong>{{ product.nome }} - ID:{{ product.id }}</strong>
+        <span>R$ {{ product.valor }}</span>
+        <span><strong>SKU: </strong>{{ product.sku }}</span>
+        <button class="danger" :disabled="deleting" @click="$emit('delete')">
+            {{ deleting ? 'Deletando...' : 'Deletar' }}
+        </button>
+    </div>
 </template>
 
 <script setup lang="ts">
-import type { ProductReponse } from '@/types/product.interface'
+import type { ProductReponse } from '@/types/product.interface';
 
 defineProps<{ product: ProductReponse; deleting: boolean }>()
 
